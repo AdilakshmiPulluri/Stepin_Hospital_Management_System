b04b79cd68ec3fed3d938abf0dd1b1ee907d0549
AdilakshmiPulluri <89721670+AdilakshmiPulluri@users.noreply.github.com>
2021-09-08 17:49:27 +0530

    Create logic.c

---
+++ Implemetations/logic.c
@@ -0,0 +1,42 @@
+void newrecord(int l)
+{
+	char q;
+	p.sn=l;
+	displaydepartment();
+	
+	cprintf("Record of patient no:");
+	printf(" %d",l);
+	
+	cprintf("Name:");
+
+	
+	cprintf("Address:");
+
+	
+	cprintf("Age: ");
+
+	
+	cprintf("Sex(m/f): ");
+
+	
+	cprintf("Disease Descrp:");
+	
+	cprintf("(In Short)");
+
+	
+	cprintf("Reff. Specialist no:");
+	fflush(stdin);
+	scanf("%[^\n]",p.name);
+	p.name[0]=toupper(p.name[0]);
+
+	fflush(stdin);
+	scanf("%[^\n]",p.address);
+
+	fflush(stdin);
+	scanf("%d",&p.age);
+	
+	fflush(stdin);
+	scanf("%c",&p.ch);
+	fflush(stdin);
+	scanf("%[^\n]",p.disease);
+}
