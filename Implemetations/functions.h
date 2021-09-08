b7b86bbe22355f660a95f2c1a3a6fc205755e0f5
AdilakshmiPulluri <89721670+AdilakshmiPulluri@users.noreply.github.com>
2021-09-08 17:36:07 +0530

    Create functions.h

---
+++ Implemetations/functions.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+void newrecord(int l);
+void print();
+void displaytest();
+void mainscreen();
+void newrecord1();
+void displaydepartment();
+void edit1();
+void editrecord();
+void switch1();
+#endif
